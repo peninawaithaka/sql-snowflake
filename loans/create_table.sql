601015469c73d51175816fd67a394de18a1f023e
Penina Waithaka <peninawaithaka5@gmail.com>
2025-07-16 12:28:09 +0300

    created warehouse and database

---
+++ loans/create_table.sql
@@ -0,0 +1,33 @@
+
+CREATE DATABASE LOANS; 
+
+CREATE TABLE "LOANS"."PUBLIC"."LOAN_PAYMENT" (
+  "Loan_ID" STRING,
+  "loan_status" STRING,
+  "Principal" STRING,
+  "terms" STRING,
+  "effective_date" STRING,
+  "due_date" STRING,
+  "paid_off_time" STRING,
+  "past_due_days" STRING,
+  "age" STRING,
+  "education" STRING,
+  "Gender" STRING);
+  
+
+
+ USE DATABASE LOANS;
+
+
+ //Loading the data from S3 bucket
+  
+ COPY INTO LOAN_PAYMENT
+    FROM s3://bucketsnowflakes3/Loan_payments_data.csv
+    file_format = (type = csv 
+                   field_delimiter = ',' 
+                   skip_header=1);
+    
+
+SELECT * FROM LOAN_PAYMENT;
+
+ 
