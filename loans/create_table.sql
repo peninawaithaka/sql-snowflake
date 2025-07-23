7e2f543e2f2d1498bf67d009818f662bc2795ce5
Penina Waithaka <peninawaithaka5@gmail.com>
2025-07-23 22:28:13 +0300

    created the aws_stage

--- loans/create_table.sql
+++ loans/create_table.sql
@@ -31,3 +31,37 @@ CREATE TABLE "LOANS"."PUBLIC"."LOAN_PAYMENT" (
 SELECT * FROM LOAN_PAYMENT;
 
  
+ --create a schema for the stages
+
+CREATE OR REPLACE SCHEMA external_stages;
+
+--create a stage - AWS S3
+CREATE OR REPLACE STAGE aws_stage
+url = 's3://bucketsnowflakes3'
+
+--describe the stage created
+desc stage aws_stage;
+
+-- list the files within the stage
+list @aws_stage;
+
+--create the table where the data will be copied into
+CREATE OR REPLACE TABLE ANALYSIS.PUBLIC.ORDERS(
+order_id varchar(30),
+amount int,
+profit int,
+quantity int,
+category varchar(30),
+subcategory varchar(30)
+)
+
+
+--loading order details data from aws_stage
+COPY INTO LOANS.PUBLIC.ORDERS
+    FROM @LOANS.EXTERNAL_STAGES.aws_stage
+    file_format = (type = csv field_delimiter= ',' skip_header=1)
+    files = ('OrderDetails.csv')
+
+
+select * from public.orders
+limit 10
